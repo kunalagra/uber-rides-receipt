4c0b792ccc02e510d2c35d75eb67f39469018049
Kunal Agrawal <66416646+kunalagra@users.noreply.github.com>
2026-03-15 21:51:11 +0000

    chore: update dependencies and improve Vite configuration
    - Bump versions of @tanstack/react-router and @tanstack/react-start
    - Upgrade @vitejs/plugin-react to version 6.0.1
    - Upgrade Vite to version 8.0.0
    - Remove vite-tsconfig-paths plugin and enable tsconfig paths in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,19 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
     nitro(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
   ],
+  resolve: {
+    tsconfigPaths: true
+  },
 })
 
 export default config
